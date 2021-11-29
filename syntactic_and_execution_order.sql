7500e1cd0f3a9c2a8fcd69c60709c9763dce7574
lat <94452456+latiful-hassan@users.noreply.github.com>
2021-11-29 15:07:41 +0000

    Add files via upload

---
+++ syntactic_and_execution_order.sql
@@ -0,0 +1,22 @@
+---------------------
+-- SYNTACTIC ORDER --
+---------------------
+
+SELECT
+FROM
+WHERE 
+GROUP BY
+HAVING 
+LIMIT
+
+---------------------
+-- EXECUTION ORDER --
+---------------------
+
+FROM
+WHERE
+GROUP BY
+HAVING 
+SELECT
+ORDER BY
+LIMIT
